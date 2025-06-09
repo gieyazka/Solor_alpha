46d4ee493220f69281e17c145db4456c5cf652e9
gieyazka <pokket.1@gmail.com>
2025-06-09 08:32:57 +0700

    200

--- app/api/test/route.js
+++ app/api/test/route.js
@@ -6,7 +6,7 @@ export async function GET(req) {
     // const res = sendMessageToLine("Test");
     // console.log("res", res);
     // return NextResponse.json({ data: res }, { status: 200 });
-    return NextResponse.json({ error: "Test" }, { status: 500 });
+    return NextResponse.json({ error: "Test" }, { status: 200 });
   } catch (error) {
     return NextResponse.json({ error: error.message }, { status: 500 });
   }
